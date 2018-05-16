900954e80474cd469b29e9ac2c01fc6e59605d31
jchamberlain909 <jchamberlain909@gmail.com>
2018-05-16 12:02:37 -0400

    implemented required methods for jukebox

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -10,3 +10,52 @@ songs = [
   "Amos Lee - Keep It Loose, Keep It Tight"
 ]
 
+def help
+  puts "I accept the following commands:"
+  puts "- help : displays this help message"
+  puts "- list : displays a list of songs you can play"
+  puts "- play : lets you choose a song to play"
+  puts "- exit : exits this program"
+end
+
+def list (song_array)
+  song_array.each_with_index do |e, i|
+    puts "#{i+1}. #{e}"
+  end
+end
+
+def play (song_array)
+  puts "Please enter a song name or number:"
+  user_input = gets.chomp
+  response = "Invalid input, please try again"
+  song_array.each_with_index do |e, i|
+    if user_input == (i+1).to_s || user_input == e
+      response = "Playing #{e}"
+    end
+  end
+  puts response
+end
+
+def exit_jukebox
+  puts "Goodbye"
+end
+
+def run(songs)
+  help
+  user_input = ""
+  while(user_input != "exit")
+    puts "Please enter a command:"
+    user_input = gets.chomp
+    case user_input
+    when "list"
+      list(songs)
+    when "play"
+      play(songs)
+    when "exit"
+      exit_jukebox
+    when "help"
+      help
+    end
+  end
+  
+end
